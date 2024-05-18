38d5e3a60c4ff3f2f01836e30830bd3f439af7f0
Varunkmr481 <kmrvarun481@gmail.com>
2024-05-19 03:06:16 +0530

    Responding to URL parameters

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-// Import the Express module ,fs module 
 const express = require('express');
 const fs = require('fs');
 
-// Create an instance of an Express application.
 const app = express();
 
 // Middleware to parse incoming JSON requests.
@@ -11,8 +9,8 @@ app.use(express.json());
 // Read and parse the tours data from the JSON file.
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
+
 // Define a route for GET requests to '/api/v1/tours'.
-// When a GET request is made to this URL, send a JSON response with the tours data.
 app.get('/api/v1/tours',(req,res)=>{
     res.status(200).json({
         status : 'success',
@@ -22,14 +20,40 @@ app.get('/api/v1/tours',(req,res)=>{
     })
 })
 
-// Define a route for GET requests to '/api/v1/tours'.
-// When a POST request is made to this URL, add a new tour to the tours data and save it to the file.
+
+// Define a route for GET requests to '/api/v1/tours/:id'.
+app.get('/api/v1/tours/:id',(req,res)=>{
+    // console.log(req.params);
+
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id == id);
+    
+    // console.log(tour);
+
+    if(tours.length < id){
+        return res.status(404).json({
+            status : "fail",
+            message : "Invalid Id"
+        });
+    }
+
+    res.status(200).json({
+        status : "success",
+        data : {
+            tour
+        }
+    })
+})
+
+
+// POST a new tour
 app.post('/api/v1/tours',(req,res)=>{
     // console.log(req.body);
+
     const newId = tours[tours.length - 1].id + 1 ;
     const newTour = Object.assign({id : newId }, req.body)
-
     tours.push(newTour);
+
     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),(err)=>{
         res.status(200).json({
             status : "success",
@@ -41,10 +65,8 @@ app.post('/api/v1/tours',(req,res)=>{
 })
 
 
-// Define the port number on which the server will listen.
+// Define the port number and start the server
 const port = 3000;
-
-// Start the server and have it listen on the specified port.
 app.listen(port,()=>{
     console.log(`App running on port Number : ${port}`);
 })
