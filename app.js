1a880575a410ef6fa0e4d28a230721aaecdf3382
Varunkmr481 <kmrvarun481@gmail.com>
2024-05-20 19:23:13 +0530

    Handle PATCH Request

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.post('/api/v1/tours',(req,res)=>{
     tours.push(newTour);
 
     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),(err)=>{
-        res.status(200).json({
+        res.status(201).json({
             status : "success",
             data : {
                 tour : newTour
@@ -64,6 +64,22 @@ app.post('/api/v1/tours',(req,res)=>{
     });
 })
 
+app.patch('/api/v1/tours/:id',(req,res)=>{
+
+    if(req.params.id * 1 > tours.length){
+        return res.status(404).json({
+            status : 'fail',
+            message : "Invalid ID given!"
+        })
+    }
+
+    res.status(200).json({
+        status : "success",
+        data : {
+            tour : '<Updated tour....>'
+        }
+    })
+})
 
 // Define the port number and start the server
 const port = 3000;
