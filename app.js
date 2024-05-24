c0e65ceaabc876bca3ed1b5de503eccdacc408a7
Varunkmr481 <kmrvarun481@gmail.com>
2024-05-25 03:03:06 +0530

    Set Environment Variable

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // MIDDLEWARES
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'));
+}
+
 app.use('/api/v1/tours',tourRouter);
 app.use('/api/v1/users',userRouter);
-app.use(morgan('dev'));
+
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req,res,next)=>{
   console.log('Hello from the middleware 😎');
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config({path : './config.env'});
+// console.log(process.env);
+
 const app = require('./app');
 
 // SERVER START
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port Number : ${port}`);
 });
