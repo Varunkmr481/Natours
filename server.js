7cbd2aa25939dc082ab8b899b91e0b96a442713d
Varunkmr481 <kmrvarun481@gmail.com>
2024-05-27 20:39:06 +0530

    Database setup and model

--- server.js
+++ server.js
@@ -18,6 +18,24 @@ mongoose
   // console.log(con.connection);
   console.log('DB Connection successful !');
 })
+
+const tourSchema = mongoose.Schema({
+  name : {
+    type : String ,
+    required : [true,"A tour must have a name!"],   //validator
+    unique : true
+  },
+  rating : {
+    type : Number,
+    default : 4.5
+  }, 
+  price : {
+    type : Number,
+    required : [true , "A tour must have a price!"]
+  }
+})
+
+const Tour = mongoose.model('Tour',tourSchema);
   
 
 // SERVER START
