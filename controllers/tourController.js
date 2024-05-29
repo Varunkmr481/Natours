0dcca735bb237be7f2630e227417cff39ec90f22
Varunkmr481 <kmrvarun481@gmail.com>
2024-05-29 18:05:37 +0530

    Implement Update/Delete Tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -35,7 +35,7 @@ exports.getAllTours = async (req, res) => {
 exports.getTour = async (req, res) => {
 
   try{
-    const tours  = await Tour.find();
+    const tours  = await Tour.findById(req.params.id);
 
     console.log(req.body.id);
     res.status(200).json({
@@ -77,19 +77,47 @@ exports.createTour = async (req, res) => {
   };
   
   
-exports.updateTour = (req, res) => {
+exports.updateTour = async (req, res) => {
   
+  try{
+
+    const tour = await Tour.findByIdAndUpdate(req.params.id , req.body , {
+        new : true ,
+        runValidators : true
+      });
+
     res.status(200).json({
       status: 'success',
       data: {
-        tour: '<Updated tour....>',
+        tour
       },
     });
+  }catch(err){
+    res.status(404).json({
+      status : 'fail',
+      message : err
+    })
+  }
   };
   
   
-exports.deleteTour = (req, res) => {
+exports.deleteTour = async (req, res) => {
 
+  try{
+
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+    
+    res.status(204).json({
+      status : 'success',
+      data : null
+    })
+
+  }catch(err){
+    res.status(404).json({
+      status : 'fail',
+      message : err
+    })
+  }
     res.status(204).json({
       status: 'success',
       data: null,
