277e0066d08efb5165fe49979f85dc184a8132f2
Varunkmr481 <kmrvarun481@gmail.com>
2024-05-29 09:19:17 +0530

    Refactoring for MVC

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
 // Read and parse the tours data from the JSON file.
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/./../dev-data/data/tours-simple.json`)
-  );
+// const tours = JSON.parse(
+//     fs.readFileSync(`${__dirname}/./../dev-data/data/tours-simple.json`)
+//   );
 
 exports.checkID = (req,res,next,val) => {
   console.log(`The value of id : ${val}`);
@@ -43,9 +43,10 @@ exports.getAllTours = (req, res) => {
     res.status(200).json({
       status: 'success',
       requestedTime : req.requestedTime ,
-      data: {
-        tours,
-      },
+      // results : tours.length,
+      // data: {
+      //   tours,
+      // }
     });
   };
   
@@ -53,35 +54,27 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
     // console.log(req.params);
     const id = req.params.id;
-    const tour = tours.find((el) => el.id == id);
-    // console.log(tour);
+    // const tour = tours.find((el) => el.id == id);
+    // // console.log(tour);
 
-    res.status(200).json({
-      status: 'success',
-      data: {
-        tour,
-      },
-    });
+    // res.status(200).json({
+    //   status: 'success',
+    //   data: {
+    //     tour,
+    //   },
+    // });
   };
   
   
 exports.createTour = (req, res) => {
     // console.log(req.body);
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
-    tours.push(newTour);
-  
-    fs.writeFile(
-      `${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),
-      (err) => {
-        res.status(201).json({
-          status: 'success',
-          data: {
-            tour: newTour,
-          },
-        });
-      }
-    );
+    
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
   };
   
   
--- server.js
+++ server.js
@@ -19,25 +19,6 @@ mongoose
   console.log('DB Connection successful !');
 })
 
-const tourSchema = mongoose.Schema({
-  name : {
-    type : String ,
-    required : [true,"A tour must have a name!"],   //validator
-    unique : true
-  },
-  rating : {
-    type : Number,
-    default : 4.5
-  }, 
-  price : {
-    type : Number,
-    required : [true , "A tour must have a price!"]
-  }
-})
-
-const Tour = mongoose.model('Tour',tourSchema);
-  
-
 // SERVER START
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
