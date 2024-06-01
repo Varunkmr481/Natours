5796fec8221566273139b842b8078f3a89e48421
Varunkmr481 <kmrvarun481@gmail.com>
2024-06-01 11:16:02 +0530

    Implement Basic filter

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -9,12 +9,33 @@ const Tour = require("./../models/tourModel");
 exports.getAllTours = async (req, res) => {
 
   try{
-    console.log(req.requestedTime);
+    //BUILD QUERY
+    const queryObj = {...req.query};
+    const excludedFields = ['page' , 'sort' , 'limit' , 'fields'];
+    excludedFields.forEach(el => delete queryObj[el] );
 
-    const tours = await Tour.find();
+    // console.log(req.query , queryObj);
 
+    // const tours = await Tour.find({
+    //   duration : 5,
+    //   difficulty : 'easy'
+    // });
+
+    // const tours = await Tour.find()
+    // .where('duration')
+    // .equals(5)
+    // .where('difficulty')
+    // .equals('easy');
+
+    //EXECUTE QUERY
+    const query = Tour.find(queryObj);
+    // console.log("Query :", query);
+
+    const tours = await query;
+
+    //SEND RESPONSE
     res.status(200).json({
-      status: 'success',
+      status: 'success', 
       requestedTime : req.requestedTime ,
       results : tours.length,
       data: {
