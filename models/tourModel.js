351bba051712f0b97d46ef17422f522a6b5a9fa9
Varunkmr481 <kmrvarun481@gmail.com>
2024-06-14 22:03:19 +0530

    Add Query Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -56,7 +56,11 @@ const tourSchema = mongoose.Schema({
       type : Date ,
       default : Date.now()
     },
-    startDates : [Date]
+    startDates : [Date],
+    secretTour : {
+      type : Boolean,
+      default : false
+    }
   },{
     toJSON : {virtuals : true},
     toObject : {virtuals : true}
@@ -83,6 +87,21 @@ const tourSchema = mongoose.Schema({
   //   console.log(doc);
   //   next();
   // })
+
+  //Query Middleware for words starting with find 
+  tourSchema.pre(/^find/, function(next){
+    // tourSchema.pre('find', function(next){
+      this.find({ secretTour : { $ne : true } });
+      this.start = Date.now();
+      next();
+    })
+
+
+  tourSchema.post(/^find/,function(docs,next){
+    console.log(`Query took ${Date.now()-this.start} milliseconds`);
+    console.log(docs);
+    next();
+  })
 
   const Tour = mongoose.model('Tour',tourSchema);
 
