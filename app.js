85078c4f1d7bc1a6d39f5e5d66f0aa5488cf2055
Varunkmr481 <kmrvarun481@gmail.com>
2024-06-16 00:39:50 +0530

    feat: Handle unhandled routes with a 404 error response

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ if(process.env.NODE_ENV === 'development'){
   app.use(morgan('dev'));
 }
 
-app.use('/api/v1/tours',tourRouter);
-app.use('/api/v1/users',userRouter);
-
-
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
@@ -24,6 +20,16 @@ app.use((req,res,next)=>{
 app.use((req,res,next)=>{
   req.requestedTime = new Date().toISOString();
   next();
+})
+
+app.use('/api/v1/tours',tourRouter);
+app.use('/api/v1/users',userRouter);
+
+app.all('*',(req,res,next)=>{
+  res.status(404).json({
+    status : 'fail',
+    message : `Can't find ${req.originalUrl} on this server`
+  })
 })
 
 module.exports = app ;
