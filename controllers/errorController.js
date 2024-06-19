97c656bc19a67c8a12f8e3d14cd978cb85641522
Varunkmr481 <kmrvarun481@gmail.com>
2024-06-19 22:24:54 +0530

    Implement differentiated error handling for development and production environments

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,42 @@
+const sendErrorDev = (res,err) => {
+  res.status(err.statusCode).json({
+    status : err.status,
+    message : err.message,
+    error : err , 
+    stack : err.stack
+  })
+}
+
+//operational , trusted errors : send message to client
+const sendErrorProd = (res,err) => {
+  if(err.isOperational){
+    res.status(err.statusCode).json({
+      status : err.status,
+      message : err.message
+    })
+  }
+
+  //programming or any unknown error : don't leak error details
+  else{
+    // 1) Log error
+    console.log('Error : ' , err);
+
+    res.status(500).json({
+      status : 'error' ,
+      message : 'Something went very wrong !'
+    })
+  }
+}
+
 module.exports = (err,req,res,next)=>{
 
     err.statusCode =  err.statusCode || 500 ;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-        status : err.status,
-        message : err.message     
-      })
+    if(process.env.NODE_ENV === 'development'){
+      sendErrorDev(res,err);
+    }
+    else if(process.env.NODE_ENV === 'production'){
+      sendErrorProd(res,err);
+    }
     }
