aa699cfcc20b8811c0e44d9ef269c9b27fcf4736
Varunkmr481 <kmrvarun481@gmail.com>
2024-06-20 06:26:00 +0530

    fix(2): Handle duplicate database fields in production environment

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,11 @@ const handleCastErrorDB = (err) =>{
   return new AppError(message,400)
 }
 
+const handleDuplicateFieldsDB = (err) => {
+  const message = `Duplicate field value : ${err.keyValue.name}. Please use another value!`
+  return new AppError(message,400);
+}
+
 const sendErrorDev = (err,res) => {
   res.status(err.statusCode).json({
     status : err.status,
@@ -49,6 +54,10 @@ module.exports = (err,req,res,next)=>{
 
       if(error.name === 'CastError'){
         error = handleCastErrorDB(error);
+      }
+
+      if(error.code === 11000){
+        error = handleDuplicateFieldsDB(error);
       }
 
       sendErrorProd(error,res);
