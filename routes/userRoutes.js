724ece53366b561d4ffaf1f2c95511c9ac379277
Varunkmr481 <kmrvarun481@gmail.com>
2024-07-06 22:38:37 +0530

    Sending emails with nodemailer

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,7 +14,7 @@ router
 router
 .post('/forgotPassword' , authController.forgotPassword);
 router
-.post('/resetPassword' , authController.resetPassword);
+.patch('/resetPassword/:token' , authController.resetPassword);
 
 router
   .route('/')
