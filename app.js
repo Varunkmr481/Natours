cc0fa699ab84676793e763a6cdd08b476f2ee3f5
Varunkmr481 <kmrvarun481@gmail.com>
2024-07-11 17:20:17 +0530

    Implement Data Sanitization

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require("./utils/appError");
 const globalErrorHandler = require('./controllers/errorController');
-const helmet = require('helmet');
 
 // 1) GLOBAL MIDDLEWARES
 
@@ -32,6 +34,11 @@ app.use("/api",limiter);
 app.use(express.json({
   limit : '10kb'
 }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS 
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
