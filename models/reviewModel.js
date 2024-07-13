8bdaf3545fae54777eddb1d445fd11bc8b7dedef
Varunkmr481 <kmrvarun481@gmail.com>
2024-07-13 21:23:31 +0530

    Populate Reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -32,6 +32,20 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+// Query middleware for populating reviews for user and tour
+reviewSchema.pre(/^find/, function(next){
+  
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  }).populate({
+    path: 'user',
+    select: 'name photo',
+  });
+
+  next();
+})
+
 const Review = mongoose.model('Review',reviewSchema);
 
 module.exports = Review;
