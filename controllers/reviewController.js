903c35ea6d5615263444f5e4e3f6005a99e45f71
Varunkmr481 <kmrvarun481@gmail.com>
2024-07-15 16:52:49 +0530

    Implement Simple Nested Routes

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -18,6 +18,11 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 exports.createReview = catchAsync(async (req,res,next)=>{
+  
+  //Allow nested routes
+  if(!req.body.tour) req.body.tour = req.params.tourId;
+  if(!req.body.user) req.body.user = req.user.id;
+
     const newReview = await Review.create(req.body);
     if(!newReview){
         return next(new AppError("The review not created due to some problem",400));
