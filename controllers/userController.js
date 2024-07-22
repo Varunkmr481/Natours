68e9b3c14b575a99c625ba86544b9d0451a7e781
Varunkmr481 <kmrvarun481@gmail.com>
2024-07-22 07:36:10 +0530

    Add a /me endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,6 +18,12 @@ const filterObj = (obj , ...allowedFields)=>{
 }
 
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
+
 exports.updateMe = catchAsync (async (req,res,next)=>{
       // 1) Create error if user tries to post password
       if(req.body.password || req.body.confirmPassword){
