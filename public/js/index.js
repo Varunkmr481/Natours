3f48df7e7043c682dc3047fcb4c6ff1cc5bc399b
Varunkmr481 <kmrvarun481@gmail.com>
2024-08-08 04:22:38 +0530

    Updating User Data

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,12 @@
 import '@babel/polyfill';  
 import { login } from './login';
 import { logout } from './login';
+import { updateData } from './updateSettings';
 
 // Dom elements 
-const loginForm = document.querySelector('.form');
+const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const updateDataForm = document.querySelector('.form-user-data');
 
 if(loginForm){
     loginForm.addEventListener('submit', e => {
@@ -19,3 +21,16 @@ if(loginForm){
 }
 
 if(logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if(updateDataForm){
+    updateDataForm.addEventListener('submit', e => {
+
+        e.preventDefault();
+        // VALUES
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+
+        updateData(name,email);
+    });
+}
+
